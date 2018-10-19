c6d6b0da198a58444d467ddd391b3bda168e002e
Camilla F Crispim <millafalconi@gmail.com>
2018-10-18 21:32:06 -0300

    Fixing typo in env vars

--- config/initializers/health_monitor.rb
+++ config/initializers/health_monitor.rb
@@ -2,7 +2,7 @@ HealthMonitor.configure do |config|
   config.database
   version = YAML.load_file("#{Rails.configuration.root}/config/version.yml")
 
-  config.environmet_variables = {
+  config.environment_variables = {
     version: version.inject('') { |c, (_k, v)| c + "#{v}." }.chop,
     migration_version: ActiveRecord::Migrator.current_version
   }
